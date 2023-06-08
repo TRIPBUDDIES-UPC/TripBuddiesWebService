fde10356ee96c13bd390f35cb4415e364cefdd08
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2023-06-08 12:09:07 -0500

    Initial commit

---
+++ src/main/java/com/upc/TRIPBUDDIES/TripbuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.upc.TRIPBUDDIES;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripbuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripbuddiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/upc/TRIPBUDDIES/TripbuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upc.TRIPBUDDIES;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripbuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
