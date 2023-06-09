b2721b29fd97800092acec1e440acac12d8a5c0a
AlessandroChincha <89095463+AlessandroChincha@users.noreply.github.com>
2023-06-09 16:11:06 -0500

    Initial Commit

--- src/test/java/com/upc/TRIPBUDDIES/TripbuddiesApplicationTests.java
+++ src/test/java/com/upc/TRIPBUDDIES/TripbuddiesApplicationTests.java
@@ -1,13 +1,15 @@
 package com.upc.TRIPBUDDIES;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
+@SpringBootApplication
 class TripbuddiesApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(TripbuddiesApplicationTests.class, args);
 	}
+
 
 }
---
+++ src/test/java/com/upc/TRIPBUDDIES/util/SwaggerConfiguration.java
@@ -0,0 +1,4 @@
+package com.upc.TRIPBUDDIES.util;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+
